49a80c8c0cc38039583b8689d3c31831b402573c
gideonco <gc@gideon.co>
2016-03-21 21:30:03 -0400

    another

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,15 +6,12 @@ var deferred = Q.defer();
 // - Youtube
 
 var discogsFetch = (function(){
-    var discogsDataCompile = [];
-
-    function populateData(ele, data) {
-        console.log('ue');
-    }
+    var discogsDataCompile = new Object();
 
     function collectDiscogsInfo(discogsLink) {
     	urlPieces = discogsLink.split("/"),
-    	discogsData = {};
+    	discogsData = new Object(),
+    	discogsMarketplace = new Object();
 
 
     	for (var prop in urlPieces) {
@@ -46,8 +43,16 @@ var discogsFetch = (function(){
 
 	    function onload() {
 	        if (request.status === 200) {
-	            deferred.resolve(request.responseText);
-	            discogsDataCompile.push(discogsResourceUrl);
+	            console.log('successful discogs api call');
+
+	            res = JSON.parse(request.response);
+
+	            discogsDataCompile['title'] = res.title;
+	            discogsDataCompile['year'] = res.year;
+	            discogsDataCompile['labels'] = res.labels;
+	            discogsDataCompile['artists'] = res.artists;
+	            discogsDataCompile['tracklist'] = res.tracklist;
+	            deferred.resolve();
 	        } else {
 	            deferred.reject(new Error("Status code was " + request.status));
 	        }
@@ -66,7 +71,7 @@ var discogsFetch = (function(){
     function collectLinks(contentElement){
     	// what about broken links, find with regex.
     	links = contentElement.getElementsByTagName('a'),
-    	collectedData = [];
+    	collectedData = new Array();
 
     	function checkForDiscogsLink(element){
     		if(element.href.includes('discogs.com')){
@@ -84,31 +89,74 @@ var discogsFetch = (function(){
     	}
     }
 
-    function createDiscogsElement(){
+    function collectMarketplaceInfo(){
+    	var request = new XMLHttpRequest();
+    	if(discogsData.type == 'masters'){
+    		marketplaceResourceUrl = 'https://api.discogs.com/marketplace/search?master_id=' + discogsData.id
+    	}
+    	if(discogsData.type == 'releases'){
+    		marketplaceResourceUrl = 'https://api.discogs.com/marketplace/search?release_id='  + discogsData.id
+    	}
 
+    	request.open("GET", marketplaceResourceUrl, true);
+	    request.onload = onload;
+	    request.onerror = onerror;
+	    request.onprogress = onprogress;
+	    request.send();
+	    function onload() {
+	    	if (request.status === 200) {
+	    		console.log('successful marketplace api call');
+	            discogsMarketplace  = JSON.parse(request.response);
+	            console.log(discogsMarketplace);
+	            deferred.resolve(discogsMarketplace);
+	        } else {
+	            deferred.reject(new Error("Status code was " + request.status));
+	        }
+	    }
+
+	    function onerror() {
+	        deferred.reject(new Error("Can't XHR " + JSON.stringify(discogsResourceUrl)));
+	    }
+
+	    function onprogress(event) {
+	        deferred.notify(event.loaded / event.total);
+	    }
+	    return deferred.promise;
     }
 
+    function createDiscogsElement(contentElement){
+    	console.log('createDiscogsElement');
+    	contentElement.insertAdjacentHTML('afterend', '<div id="COLLECT"><div id="information-DISCOGS"></div><div id="marketplace-DISCOGS"></div></div>');
+    }
+
+    function createMarketplaceModule(contentElement){
+    	console.log('createMarketplaceModule');
+    	var info = document.getElementById("information-DISCOGS");
+    	console.log(discogsMarketplace);
+    	// select 
+    	
+    	
+    	
+    }
+
+
   	function initialize( contentElement ) {
-  		createDiscogsElement();
   		discogsLinks = collectLinks(contentElement);
   		if(discogsLinks){
-	        collectDiscogsInfo(discogsLinks[0]).then(function(){
-	        	  // create a new div element 
-				  // and give it some content 
-				  var newDiv = document.createElement("div"); 
-				  var newContent = document.createTextNode("Hi there and greetings!"); 
-				  newDiv.appendChild(newContent); //add the text node to the newly created div. 
-
-				  // add the newly created element and its content into the DOM 
-				  document.body.insertAfter(newDiv, contentElement); 
+	        collectDiscogsInfo(discogsLinks[0])
+	        .then(function(){
+	        	createDiscogsElement(contentElement);
+	        })
+	        .then(collectMarketplaceInfo)
+	        .then(function(){
+        		createMarketplaceModule(contentElement);
 	        });
   		}
     }
 
     return {
-        data: discogsDataCompile,
         init: initialize
-    }; 
+    };
 })();
 
 
